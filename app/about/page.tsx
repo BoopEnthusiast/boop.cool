eace34f06aafc3a06f4518ca00d9df63c42ab964
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-07-09 22:27:10 +1200

    Header stuff

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 
+export const metadata = {
+    title: 'About',
+}
+
 export default function About() {
-  return (
-    <>
-      <h1>About</h1>
-      <div>
-        Back to <Link href="/">Home</Link>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h1>About</h1>
+            <div>
+                Back to <Link href="/">Home</Link>
+            </div>
+        </>
+    );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,37 @@
 import './global.css'
 
+
+export const metadata = {
+    title: {
+      template: '%s | Boop\'s kool I swear',
+      default: 'Be kool, be Boop',
+    },
+    description: {
+        template: '%s - Held on Boop\'s coolest domain',
+        default: 'A personal website from Boop the internet user, welcome to my coolest domain.',
+    },
+    generator: 'Next.js',
+    applicationName: 'Boop\'s Kewl site',
+    keywords: ['Epic', 'Cool', 'Boop', 'Gamer'],
+    authors: [{ name: 'Boop', url: 'https://boop.website' }],
+    creator: 'Boop',
+    publisher: 'Boop',
+    formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+    },
+}
+
+
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
 }
