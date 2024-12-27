2e6505ea4441f7c3c2dad2e8c0ca83068caf927c
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-12-27 15:41:13 +1300

    Sizing and optimizing font

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import './home.css'
 const Home = () => {
     return (
         <>
-            <h1 id='title'>Directory of my cool stuff</h1>
-            <p>Hello World! This is the Home page</p>
-            Visit the <Link href="/about">About</Link> page.
+            <h1>
+                Boop's Portfolio Site
+            </h1>
         </>
     );
 };
