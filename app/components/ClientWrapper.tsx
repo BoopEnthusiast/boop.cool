0d128b8d604a9cdcfcacff7f5d6bbcf9d444f924
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-12-27 22:13:15 +1300

    THIS GOES TO GITHUB IT WORKS BUT THERE'S A MAJOR VULNERABILITY

---
+++ app/components/ClientWrapper.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+
+function Box() {
+  return (
+    <mesh>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshPhongMaterial />
+    </mesh>
+  )
+}
+
+export default function ClientWrapper() {
+  return (
+    <div className="w-full h-screen">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Box />
+          <OrbitControls />
+          <ambientLight intensity={0.1} />
+          <directionalLight position={[0, 0, 5]} color="red" />
+        </Suspense>
+      </Canvas>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,27 @@ import './home.css'
 import Header from "./components/header";
 import App from "./components/three";
 
+// app/page.tsx
+import dynamic from 'next/dynamic'
 
+const ClientWrapper = dynamic(() => import('./components/ClientWrapper'), {
+  ssr: false,
+  loading: () => <div>Loading 3D scene...</div>
+})
 
 const Home = () => {
     return (
         <>  
             <div>
                 <Header />
-                <App />
+                <ClientWrapper />
             </div>
         </>
     );
 };
+
+
+
 
 
 export default Home;
