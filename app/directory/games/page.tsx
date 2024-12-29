e01c4b39faaee23ce02638f4df38c7ef3f4d35f7
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-12-30 09:33:09 +1300

    Directory styles working

--- app/directory/games/page.tsx
+++ app/directory/games/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 
 export const metadata = {
-    title: 'About',
+    title: 'Games',
 }
 
 
--- app/directory/layout.tsx
+++ app/directory/layout.tsx
@@ -1,1 +1,5 @@
-import './directory.css'
+import styles from './directory.module.css'
+
+export default function Page({ children }: { children: React.ReactNode }) {
+    return <main className={styles.blog}>{children}</main>
+  }
