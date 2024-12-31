6ee33e9beb601d257bd25c41426cf8953067e684
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-12-31 14:17:43 +1300

    Tragedy!_ Game dialogue

--- app/directory/games/page.tsx
+++ app/directory/games/page.tsx
@@ -11,13 +11,14 @@ export default function Games() {
                 <Parallax pages={3} style={{ top: '0', left: '0' }}>
                     <ParallaxLayer offset={0} speed={1.9}>
                         <div className='parallax-bg'>
-                                <img src={'/games/Games.jpg'} alt='background image' />
+                                <img src={'/games/Games.jpg'} alt='background image with pink keyboard, two index controllers, and a pink mouse' />
                         </div> 
                     </ParallaxLayer>
                     <ParallaxLayer offset={0} speed={1}>
                         <div className='boxes'>
                             <h1>Games</h1> <br />
-                            <PresentBox />
+                            <PresentBox image='/games/Tragedy.png' imageAlt='The text "Tragedy!_" in off-white on off-black' title='Tragedy!_'
+                            description="A university project for GAME204: Writing For Games. It has a branching narrative, with 5 paths, on 3 different layers of story. The layers of story are connected, but you, the player, must unravel the truth of the situation. It is a text-based narrative game made in Ink."/>
                             <PresentBox />
                             <PresentBox /> 
                             <PresentBox /> 
