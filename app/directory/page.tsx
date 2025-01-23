bca543f3475631cab0a5bd5c30be5c8454dfe5b7
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-01-23 23:13:54 +1300

    add code to directory

--- app/directory/page.tsx
+++ app/directory/page.tsx
@@ -8,21 +8,27 @@ export default function Directory() {
     return (
         <>
             <h1>Directory</h1><br />
-            <DirectoryBox title="Games" description="All of the released games I've made." link="/directory/games"
-            items={[
-                'Reverie Memory', 
-                'Snakerosion',
-                'Tetraduel',
-                'Tragedy!_',
-                'Cozy Potion Evolution',
-                'Mycelium Melody',
-                'Space To Repair',
-                'Flower Tower',
-                "Breaking Partner's Dates",
-                'Espionage Office',
-                'Mini Necro',
-                'Zephyrinth',
+            <div className="directoryBoxHolder">
+                <DirectoryBox title="Games" description="All of the released games I've made." link="/directory/games"
+                items={[
+                    'Reverie Memory', 
+                    'Snakerosion',
+                    'Tetraduel',
+                    'Tragedy!_',
+                    'Cozy Potion Evolution',
+                    'Mycelium Melody',
+                    'Space To Repair',
+                    'Flower Tower',
+                    "Breaking Partner's Dates",
+                    'Espionage Office',
+                    'Mini Necro',
+                    'Zephyrinth',
                 ]}/>
+                <DirectoryBox title="Code Examples" description="Snapshots of my programming." link="/directory/code"
+                items={[
+                    'Programming Language'
+                ]}/>
+            </div>
         </>
     )
 }
